c441c6ce7c30986e5907e9fd287c813d89db1bcc
kin <H.incapp1p@gmail.com>
2018-11-07 17:46:18 +0800

    update

--- src/App.js
+++ src/App.js
@@ -12,38 +12,35 @@ import fs from 'react-native-fs';
 import Realm from 'realm';
 
 const RemediesSchema ={
-    name : 'Remedies',
-    properties:{
-     id:'string',
-     remedy:'string',
-     concomitants:'string',
-     location:'string',
-     aetiology:'string',
-     modalities:'string',
-     synopsis:'string',
-     keynote:'string',
-     constitutional:'string',
+    name: 'Remedies',
+    properties: {
+        id: 'string?',
+        remedy: 'string?',
+        concomitants: 'string?',
+        location: 'string?',
+        aetiology: 'string?',
+        modalities: 'string?',
+        synopsis: 'string?',
+        keynote: 'string?',
+        constitutional: 'string?'
+
     }
 };
+fs.copyFileAssets('home.realm', fs.DocumentDirectoryPath + '/home.realm').
+then(()=>{
+    const realm =new Realm({
+        path: Platform.OS === 'ios'
+            ? fs.MainBundlePath + '/home.realm'
+            : fs.DocumentDirectoryPath + '/home.realm',
+        schema: [RemediesSchema],
+        readOnly:true
+    });
+    const remedies = realm.objects('Remedies');
+    console.log(remedies.length);
+});
+
 
 export default class App extends Component {
-
-    componentWillMount(){
-  fs.copyFileAssets('home.realm', fs.DocumentDirectoryPath + '/home.realm').
-   then(()=>{
-       console.log(fs.copyFileAssets("done"));
-       const realm =new Realm({
-           path: Platform.OS === 'ios'
-               ? fs.MainBundlePath + '/home.realm'
-               : fs.DocumentDirectoryPath + '/home.realm',
-           schema: [RemediesSchema],
-           readOnly:true
-       });
-       const remedies = realm.objects('Remedies');
-       console.log(remedies.length);
-   });
-};
-
 
 render() {
 return (
@@ -61,7 +58,7 @@ container: {
 flex: 1,
 justifyContent: 'center',
 alignItems: 'center',
-backgroundColor: '#F5FCFF',
+backgroundColor: '#FFF',
 },
 welcome: {
 fontSize: 20,
