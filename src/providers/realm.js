df26f285b8597f33cdb754c338e2b71539ee56b4
Anakin <genzokin@hotmail.com>
2018-11-09 01:04:53 +0800

    update

--- src/providers/realm.js
+++ src/providers/realm.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View,AppRegistry} from 'react-native';
+import {Platform, StyleSheet, Text, View} from 'react-native';
 import fs from 'react-native-fs';
 import Realm from 'realm';
 
