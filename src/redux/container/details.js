f02e4176d102093a9efa940cfc3dea5751f0aaad
Anakin <genzokin@hotmail.com>
2018-12-13 02:31:22 +0800

    update

--- src/redux/container/details.js
+++ src/redux/container/details.js
@@ -2,43 +2,59 @@
 
 import React, { Component } from 'react';
 import {StyleSheet, Text, View,ScrollView} from 'react-native';
-import PropTypes from 'prop-types';
 import { Card } from 'react-native-elements';
+import Realm from "realm";
+import {allSchemas} from "../../database/allSchema";
 
  class details extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+           result: [],
+        };
     }
 
     componentWillMount(){
-        const id = this.props.data || 'No Data';
+        const id = this.props.data_id || 'No Data';
         console.log(id+"receive");
+        Realm.open(allSchemas)
+            .then(realm => {
+                this.setState({
+                    result : realm.objects("Remedies").filtered('id = "104"'),
+                });
+                //realm.close();
+            }
+            ).catch(error => {
+            console.log(error);
+
+        })
+
     }
 
 
     render() {
         return (
             <ScrollView style={{flex: 1}}>
-               /** <Card
-                    title={this.props.items.item.remedy}>
+               <Card
+                    title={this.state.result.remedy}>
                     <Text style={styles.textTitle}>Comcomitants :
-                        <Text style={styles.contextText}> {this.props.items.item.concomitants}</Text></Text>
+                        <Text style={styles.contextText}> {this.state.result.concomitants}</Text></Text>
 
                     <Text style={styles.textTitle}>Location :
-                        <Text style={styles.contextText}> {this.props.items.item.location}</Text></Text>
+                        <Text style={styles.contextText}> {this.state.result.location}</Text></Text>
 
                     <Text style={styles.textTitle}>Aetiology :
-                        <Text style={styles.contextText}> {this.props.items.item.aetiology}</Text></Text>
+                        <Text style={styles.contextText}> {this.state.result.aetiology}</Text></Text>
 
                     <Text style={styles.textTitle}>Modalities :
-                        <Text style={styles.contextText}>{this.props.items.item.modalities}</Text></Text>
+                        <Text style={styles.contextText}>{this.state.result.modalities}</Text></Text>
 
                     <Text style={styles.textTitle}>Synopsis :
-                        <Text style={styles.contextText}>{this.props.items.item.synopsis}</Text></Text>
+                        <Text style={styles.contextText}>{this.state.result.synopsis}</Text></Text>
 
                     <Text style={styles.textTitle}>Keynote :
-                        <Text style={styles.contextText}>{this.props.items.item.keynote}</Text></Text>
-                </Card>**/
+                        <Text style={styles.contextText}>{this.state.result.keynote}</Text></Text>
+                </Card>
             </ScrollView>
 
         );
