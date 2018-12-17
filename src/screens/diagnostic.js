1c421ebf7ffbb3e58fa42121f4f483a189b59eb5
Anakin <genzokin@hotmail.com>
2018-12-17 12:28:31 +0800

    update

--- src/screens/diagnostic.js
+++ src/screens/diagnostic.js
@@ -1,15 +1,35 @@
 'use strict';
 
 import React, { Component } from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, ImageBackground, StyleSheet, Dimensions} from 'react-native';
 
 export default class diagnostic extends Component {
     render() {
         return (
-            <View>
-                <Text>archive</Text>
+            <View style={styles.container}>
+                <ImageBackground
+                    source={require('../icons/homeopathy_sym.png')}
+                    style={styles.backgroundImage}>
+                    <Text>
+                        Some text here ...
+                    </Text>
+                </ImageBackground>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    backgroundImage:{
+        flex : 1,
+        width : '100%'
+    }
+});
+
+
 
