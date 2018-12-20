056439fc4afed9a08b3791625dab66159388d656
Anakin <genzokin@hotmail.com>
2018-12-21 03:16:13 +0800

    update

--- src/App.js
+++ src/App.js
@@ -11,10 +11,15 @@
 import React, { Component } from 'react';
 import Router from './Router';
 import Realm from "realm";
+import fs from "react-native-fs";
+import {Platform} from 'react-native';
 
 
 class App extends React.Component {
     render() {
+        fs.unlink(Platform.OS === 'ios'
+            ? fs.MainBundlePath + '/home.realm'
+            : fs.DocumentDirectoryPath + '/home.realm');
         Realm.copyBundledRealmFiles();
         return (
             <Router />
