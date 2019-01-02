a9f0adf393a80d375c768c017f06ee8996aae822
Anakin <genzokin@hotmail.com>
2019-01-03 01:00:48 +0800

    update

--- src/screens/about.js
+++ src/screens/about.js
@@ -7,7 +7,12 @@ export default class About extends Component {
     render() {
         return (
             <View>
-                <Text>TBC</Text>
+                <Text>本程式只用作學術交流,而且不會上架,只用作封測測試
+                    如對內容有疑問,請參考AHI Note.所有資料以課程筆記為準
+                    任何人未經著作者同意,不得進行任何發佈.如有發現,會即時關閉封測.
+                    及知會有關人士.
+                    謝謝
+                </Text>
             </View>
         );
     }
